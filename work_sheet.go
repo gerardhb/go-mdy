98d29ba3a39c4ac16ebb9e967321d3416b9e3666
zhanghaibo <zhanghaibo@uino.com>
2022-11-06 11:18:21 +0800

    优化appKey和sign代码

--- work_sheet.go
+++ work_sheet.go
@@ -8,26 +8,23 @@ type WorkSheetRequest struct {
 	Req *resty.Request
 }
 
-// WorkSheetDTO 工作表信息请求DTO
-type WorkSheetDTO struct {
-	AppKey      string `json:"appKey"`
-	Sign        string `json:"sign"`
-	WorksheetId string `json:"worksheetId"`
+type BaseDTO struct {
+	AppKey string `json:"appKey"`
+	Sign   string `json:"sign"`
 }
 
-// ListDTO 筛选器
-type ListDTO struct {
-	AppKey       string   `json:"appKey"`      // 必填
-	Sign         string   `json:"sign"`        // 签名 必填
-	WorksheetId  string   `json:"worksheetId"` // 工作表ID 必填
-	ViewId       string   `json:"viewId"`      // "视图ID,必填"
-	PageSize     int      `json:"pageSize"`    // 必填
-	PageIndex    int      `json:"pageIndex"`   // 必填
-	SortId       string   `json:"sortId"`      // "排序字段ID"
-	IsAsc        bool     `json:"isAsc"`       // "是否升序"
-	Filters      []Filter `json:"filters"`
-	NotGetTotal  string   `json:"notGetTotal"`  //"是否不统计总行数以提高性能"
-	UseControlId string   `json:"useControlId"` //"是否只返回controlId，默认false"
+func (b *BaseDTO) SetSign(sign string) {
+	b.Sign = sign
+}
+
+func (b *BaseDTO) SetAppKey(appKey string) {
+	b.AppKey = appKey
+}
+
+// WorkSheetDTO 工作表信息请求DTO
+type WorkSheetDTO struct {
+	BaseDTO
+	WorksheetId string `json:"worksheetId"`
 }
 
 // WorkSheetInfo 代表明道云的每个工作表信息
@@ -101,13 +98,24 @@ type WorkSheetInfo struct {
 	} `json:"controls"`
 }
 
+func newWorkSheetDTO(appKey, sign, worksheetId string) *WorkSheetDTO {
+	return &WorkSheetDTO{
+		BaseDTO: BaseDTO{
+			AppKey: appKey,
+			Sign:   sign,
+		},
+		WorksheetId: worksheetId,
+	}
+}
+
 // GetWorksheetInfo 获取工作表结构信息 POST
 func (r *WorkSheetRequest) GetWorksheetInfo(worksheetId string) (*WorkSheetInfo, error) {
-	response, err := r.Req.SetResult(&Response[WorkSheetInfo]{}).SetBody(&WorkSheetDTO{
-		AppKey:      r.mdy.appKey,
-		Sign:        r.mdy.sign,
+	dto := &WorkSheetDTO{
 		WorksheetId: worksheetId,
-	}).Post(GetWorksheetInfoURL)
+	}
+	r.Set(dto)
+	response, err := r.Req.SetResult(&Response[WorkSheetInfo]{}).
+		SetBody(dto).Post(GetWorksheetInfoURL)
 	if err != nil {
 		return nil, err
 	}
@@ -116,15 +124,31 @@ func (r *WorkSheetRequest) GetWorksheetInfo(worksheetId string) (*WorkSheetInfo,
 }
 
 // GetFilterRows 获取列表 POST
-func (r *WorkSheetRequest) GetFilterRows(worksheetId string) (*WorkSheetInfo, error) {
-	response, err := r.Req.SetResult(&Response[WorkSheetInfo]{}).SetBody(&WorkSheetDTO{
-		AppKey:      r.mdy.appKey,
-		Sign:        r.mdy.sign,
-		WorksheetId: worksheetId,
-	}).Post(GetWorksheetInfoURL)
+func (r *WorkSheetRequest) GetFilterRows(filter *FilterRowDTO) (*PageRow, error) {
+	r.Set(filter)
+	response, err := r.Req.SetResult(&Response[PageRow]{}).SetBody(filter).Post(GetFilterRowsURL)
 	if err != nil {
 		return nil, err
 	}
-	result := response.Result().(*Response[WorkSheetInfo])
+	result := response.Result().(*Response[PageRow])
 	return &result.Data, nil
+}
+
+// AddRow 新建行记录 POST
+// return 记录的id
+func (r *WorkSheetRequest) AddRow(row *RowDTO) (string, error) {
+	r.Set(row)
+	response, err := r.Req.SetResult(&Response[string]{}).SetBody(row).Post(AddRowURL)
+	if err != nil {
+		return "", err
+	}
+	result := response.Result().(*Response[string])
+	return result.Data, nil
+}
+
+func (r *WorkSheetRequest) Set(info any) {
+	if sign, ok := info.(Signer); ok {
+		sign.SetSign(r.mdy.GetSign())
+		sign.SetAppKey(r.mdy.GetAppKey())
+	}
 }
