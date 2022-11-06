9e64d77936e2adba290702b094eb24b2fdc1749b
zhanghaibo <zhanghaibo@uino.com>
2022-11-06 11:17:10 +0800

    增加测试

--- mdy_test.go
+++ mdy_test.go
@@ -7,5 +7,6 @@ func TestNew(t *testing.T) {
 }
 
 func NewMdy() *mdy {
-	return New("384ed23690c2bee1", "", "NTViMmFjNGRhMDQ4NTQzNjhjNGFmZDQwMzRmM2VmZTM2YzYwYTVlMjcxMGE3YmVmMTZiZmU5ODk4NWRiMTQ2Nw==")
+	return New("384ed23690c2bee1", "NTViMmFjNGRhMDQ4NTQzNjhjNGFmZDQwMzRmM2VmZTM2YzYwYTVlMjcxMGE3YmVmMTZiZmU5ODk4NWRiMTQ2Nw==", "").WithDebug()
+	//return New("2ba7c7dcc8c433ad", "YmZjOTNjMjhkNTJhOTI1ZmRmMzdiOWZkMDBkYzIyYmZlMWE4ODk4MWM5MDU1NWYwNWFiNDdjNjMyMzkyZTBjZg==", "").WithDebug()
 }
